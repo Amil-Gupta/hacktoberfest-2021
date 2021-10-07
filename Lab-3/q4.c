4c9272ac3b0de462da0957e31b4282551fae2e39
TheVivekRajput <72144185+TheVivekRajput@users.noreply.github.com>
2021-10-07 14:32:41 +0530

    Create q4.c

--- Lab-3/q4.c
+++ Lab-3/q4.c
@@ -1,11 +1,11 @@
-#include<stdio.h>
+#include<stdio.h>//Importing the header file for standard input and output operations.
 
-int find_max_sum(int *a, int n)
+int find_max_sum(int *a, int n)//The find_max_sum function to find the max values which takes 2 parameters.
 {
     int i, j, S=0, sum=0;
     for(i=0; i<n; i++)
     {
-        sum=0;
+        sum=0;//initializing the sum variable.
         for(j=i; j<n; j++)
         {
             sum += a[j];
@@ -22,14 +22,15 @@ int main()
 {
     int n, i;
     printf("Enter no. of elements\n");
-    scanf("%d", &n);
+    scanf("%d", &n);//Accepting array size in the variable n.
     int a[n];
     printf("Enter array elements\n");
+    //Accepting element of array.
     for(i=0; i<n; i++)
     {
         scanf("%d", &a[i]);
     }
-    printf("Maximum subarray sum(S) = %d\n", find_max_sum(a, n));
+    printf("Maximum subarray sum(S) = %d\n", find_max_sum(a, n));//Printing the Maximum subarray sum(s).
 
     return 0;
 }
